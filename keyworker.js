1a6f47e576c20ba360ce16856e74477edc9b9eaa
Jara Santamaria <jarasmar@gmail.com>
2020-06-01 16:19:47 +0100

    create a keyworker constructor

---
+++ keyworker.js
@@ -0,0 +1,10 @@
+class KeyWorker {
+  constructor() {
+    this.x = 50;
+    this.y = height - 50;
+  }
+
+  draw() {
+    rect(this.x, this.y, 50, 50);
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
-function setup() {
-  createCanvas(600, 200);
+let keyWorker;
 
+function setup() {
+  createCanvas(600, 450);
+  keyWorker = new KeyWorker();
 }
 
 function draw() {
   background(168);
-
+  keyWorker.draw();
 }
