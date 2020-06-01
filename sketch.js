817887ff371853d0b5f94f987b21587bf46b66a6
plkujaw <jawor.jakub@gmail.com>
2020-06-01 15:19:29 +0100

    new setup

--- sketch.js
+++ sketch.js
@@ -1,30 +1,9 @@
-let yVelocity;
-
-
 function setup() {
   createCanvas(600, 200);
-  yVelocity = 0;
+
 }
 
 function draw() {
   background(168);
-
-  fill('#FF00FF')
-  ellipse(20, 20, 40);
-
-  handleKey();
-}
-
-
-
-function handleKey() {
-  if (keyIsDown(UP_ARROW) || keyIsDown(32) || mouseIsPressed) {
-
-    if (onGround) {
-      yVelocity += 5;
-      console.log('onGround');
-    }
-
-  }
 
 }
