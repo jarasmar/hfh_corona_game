2e903de025ae2166356892a5881215346c455a8a
plkujaw <jawor.jakub@gmail.com>
2020-06-01 14:50:07 +0100

    add handleKey and draw a ball

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
-function setup(){
-createCanvas(600,200);
+let yVelocity;
+
+
+function setup() {
+  createCanvas(600, 200);
+  yVelocity = 0;
 }
 
-function draw(){
+function draw() {
   background(168);
+
+  fill('#FF00FF')
+  ellipse(20, 20, 40);
+
+  handleKey();
+}
+
+
+
+function handleKey() {
+  if (keyIsDown(UP_ARROW) || keyIsDown(32) || mouseIsPressed) {
+
+    if (onGround) {
+      yVelocity += 5;
+      console.log('onGround');
+    }
+
+  }
+
 }
