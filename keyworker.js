130fef275eb4f28739fd7772bd95ceeedd6acb13
your Shubinder Virk <shubinder92@gmail.com>
2020-06-02 13:40:08 +0100

    Can change the character by selecting the drop down

--- keyworker.js
+++ keyworker.js
@@ -1,14 +1,29 @@
  class KeyWorker {
   
   constructor () {
-    this.size = 50;
+    this.size = 150;
     this.x = 50;
     this.y = height - this.size;
     this.yVelocity = 0;
     this.gravity = 2;
     this.score = 0;
     this.level = 0;
-    this.keyworker = keyworkerImg
+    this.keyworker = maleDoctor
+  }
+
+// function preload(){
+// }
+
+changeCharacter(){
+    if(dropdown.value() === "Female Doctor"){
+      this.keyworker = femaleDoctor;
+    }
+    if(dropdown.value === "Male Doctor"){
+      this.keyworker = maleDoctor
+    }
+    draw()
+    console.log("Chosen KeywWorker", this.keyworker)
+
   }
 
   jump() {
@@ -25,10 +40,10 @@
     }
   }
 
-  changeCharacter(){
-    this.keyworker = dropdown.value();
-    console.log("Button is pressed");
-    console.log("Chosen Keyworker", this.keyworker)
+  move() {
+    this.y += this.yVelocity;
+    this.yVelocity += this.gravity;
+    this.y = constrain(this.y, 0, height - this.size);
   }
 
   hits(virus){
@@ -36,13 +51,8 @@
                           virus.x, virus.y ,virus.size, virus.size);
   }
 
-  move() {
-    this.y += this.yVelocity;
-    this.yVelocity += this.gravity;
-    this.y = constrain(this.y, 0, height - this.size);
-  }
-
   draw() {
     image(this.keyworker, this.x, this.y, this.size, this.size);
+    console.log("Keyworker Draw Run")
   }
 }
--- sketch.js
+++ sketch.js
@@ -5,11 +5,13 @@ let input;
 let keyworkerImg;
 let virusImg;
 let backgroundImg;
+let femaleDoctor;
 
 function preload(){
-  keyworkerImg = loadImage('mariodoctor.jpeg');
   virusImg = loadImage('coronavirus.jpeg');
   backgroundImg = loadImage("London.jpeg")
+  maleDoctor = loadImage('mariodoctor.jpeg');
+  femaleDoctor = loadImage('doctor-female.png');
 }
 
 function setup() {
@@ -20,11 +22,9 @@ function setup() {
     // Position the dropdown menu 
     dropdown.position(350,90); 
     // Set options 
-    dropdown.option("Doctor"); 
-    dropdown.option("Fireman"); 
-    dropdown.option("SuperShopWorker"); 
-    dropdown.option("Paramedic"); 
-    button = createButton('submit');
+    dropdown.option("Male Doctor"); 
+    dropdown.option("Female Doctor"); 
+    button = createButton('Choose');
     button.position(380,120); 
     button.mousePressed(()=>{
         keyWorker.changeCharacter()
@@ -45,16 +45,19 @@ function draw() {
   background(backgroundImg);
   text(`Score: ${keyWorker.score}`, 10, 10, 70, 80);
   text(`Level: ${keyWorker.level}`, 740, 10, 70, 80);
-  text(dropdown.value(), 540, 10, 70, 80);
+  text(dropdown.value(), 340, 10, 70, 80);
 
   for(let v of virus) {
     v.move();
     v.draw();
     if(keyWorker.hits(v)){
       console.log("Game Over");
+      text("Game Over", 240, 500, 100, 80);
       noLoop(); 
     }
   }
   keyWorker.draw();
+    console.log("Sketch Draw Run")
   keyWorker.move();
+    console.log("Sketch Move Run")
 }
